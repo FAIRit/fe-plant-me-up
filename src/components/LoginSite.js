ea126c4b46ee6cd02ff75325df7182c039f3846f
Katarzyna Sitarz <turrrbokitty@gmail.com>
2020-04-03 15:45:43 +0200

    WIP some css to log-in form

--- src/components/LoginSite.js
+++ src/components/LoginSite.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { firebase, auth } from "../firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 export class LoginSite extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: ""
+  };
 
   handleChange = e => {
     this.setState({
-      text: e.target.value
+      [e.target.name]: e.target.value
     });
   };
 
@@ -21,24 +25,27 @@ export class LoginSite extends Component {
           <form onSubmit={this.handleSubmit} className="c-login-form">
             <input
               type="text"
-              name="userText"
-              placeholder="user"
+              name="email"
+              placeholder="e-mail"
               onChange={this.handleChange}
-              value={this.state.text}
+              value={this.state.email}
               cols={40}
             />
             <br />
             <input
               type="text"
-              name="passText"
+              name="password"
               placeholder="hasło"
               onChange={this.handleChange}
-              value={this.state.text}
+              value={this.state.password}
               cols={40}
             />
             <br />
             <button className="btn login-btn">zaloguj</button>
           </form>
+          <h4>
+            <Link to={"/registrationSite"}>Nie masz konta? Załóż je!</Link>
+          </h4>
         </div>
       </>
     );
